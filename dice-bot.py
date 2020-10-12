f2fbadaef0283f0ff99c364deea22bde330219cb
Sogranar <haeshi@rambler.ru>
2020-10-12 07:49:13 +0300

    added !card command and almost all game cards - items, heroes etc

--- dice-bot.py
+++ dice-bot.py
@@ -3,21 +3,22 @@ import random
 import pickle as pkl
 import numpy as np
 import os
-from dotenv import load_dotenv
+#import requests
+import glob
 
+from dotenv import load_dotenv
+#from bs4 import BeautifulSoup
 
 load_dotenv(verbose=True)
-
-
 
 client = discord.Client()
 
 # каждый кубик атаки и силы представляет собой список из 6 массивов, каждый из которых отражает одну грань, где первое значение массива - дальность, второе - урон, третье - заряды
-blue = [[0,0,0],[2,2,1],[3,2,0],[4,2,0],[5,1,0],[6,1,1]]
+blue = [[0,0,0],[2,2,1],[3,2,0],[4,2,0],[5,1,0],[6,1,1],'blue']
 
-green = [[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[1,1,1]]
-yellow = [[1,1,0],[1,0,1],[2,1,0],[0,2,0],[0,1,1],[0,2,1]]
-red = [[0,1,0],[0,2,0],[0,2,0],[0,2,0],[0,3,0],[0,3,1]]
+green = [[0,1,0],[0,0,1],[1,1,0],[1,0,1],[0,1,1],[1,1,1],'green']
+yellow = [[1,1,0],[1,0,1],[2,1,0],[0,2,0],[0,1,1],[0,2,1],'yellow']
+red = [[0,1,0],[0,2,0],[0,2,0],[0,2,0],[0,3,0],[0,3,1],'red']
 
 brown = [0,0,0,1,1,2]
 gray = [0,1,1,1,2,3]
@@ -30,12 +31,11 @@ stat_input = open('stat.pkl', 'rb')
 stat = pkl.load(stat_input)
 stat_input.close()
 
-
-#def result_print(color, color_text, stat_number, mess):
-#    i = 0
-#    d = random.randint(0,5)
-#    if np.sum(color[d]) == 0 : await mess.channel.send(':regional_indicator_x: (miss)')
-#    await mess.channel.send(':{}_square: B: {} :archery: + {} :heart: + {} :zap:'.format(color_text,color[d][0],color[d][1],color[d][2]))
+##to do throw and message as a separate class
+def roll(dice):
+    corner = random.randint(0,5)
+    text = ':{}_square: {}: {} :archery: + {} :heart: + {} :zap:'.format(dice[6],dice[6][0].upper(),dice[corner][0],dice[corner][1],dice[corner][2])
+    return corner,text
 
 def att_sum(sum, dice, corner):
     i = 0
@@ -48,16 +48,28 @@ def att_sum(sum, dice, corner):
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
+#@client.event
+#async def on_message(message):
+#
+#    command = message.content
+#    command = command.split(' ')
+#    if (command[0] == '!item') :
+#        req = requests.get('https://descent2e.fandom.com/wiki/{}'.format(command[1]))
+#        parse = BeautifulSoup(req.text, 'html.parser')
+#        image = parse.find("meta", property="og:image").get("content")
+#        await message.channel.send(image)
+#    else : return
+
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.name != 'bots':
-        return
-
-#    if message.author.name == 'Хаёши':
-#        await message.channel.send('Hello, creator!')
+    #if message.channel.name != 'bots':
+    #    return
 
     global stat
     stat_output = open('stat.pkl', 'wb')
@@ -67,43 +79,58 @@ async def on_message(message):
     blue_error = 0
     die_pool = message.content
     die_pool = die_pool.split(' ')
+    command = die_pool
+
+    if (command[0] == '!card') :
+        i = 2
+        img_name = command[1].lower()
+        while i < len(command) :
+            img_name += '-' + command[i].lower()
+            i += 1
+        path = './cards/**/{}.*'.format(img_name)
+        if (glob.glob(path)) == [] : await message.channel.send('Nothing found')
+        img_path = glob.glob(path)
+        for img in [0, 1, 2, 3, 4]:
+            if ((img_path[img] == './cards/lieutenants/baron-zachareth-act1.jpg') | (img_path[img] == './cards/agents/baron-zachareth-agent-act1.jpg')) : await message.channel.send('This is not even my final form!')
+            if ((img_path[img] == './cards/lieutenants/baron-zachareth-act2.jpg') | (img_path[img] == './cards/agents/baron-zachareth-agent-act2.jpg')) : await message.channel.send('Tremble before me, pityfull heroes!')
+            await message.channel.send(file=discord.File(img_path[img]))
+        if (len(img_path) > 5): await message.channel.send('Too many results')
+
+    if message.channel.name != 'bots': return
+
     for dice in die_pool:
         if ((dice == 'blue') | (dice == 'b') | (dice == 'с')):
-            i = 0
             if (blue_count == 1) :
                 if (blue_error == 0) : await message.channel.send(':exclamation: You can only use one blue die in a single throw!')
                 blue_error += 1
                 continue
-            blue_count += 1
-            d = random.randint(0,5)
-            stat[0][d] += 1
-            if (d == 0) : await message.channel.send(':regional_indicator_x: (miss)')
-            elif (die_pool[0] != 'hide') : await message.channel.send(':blue_square: B: {} :archery: + {} :heart: + {} :zap:'.format(blue[d][0],blue[d][1],blue[d][2]))
-            att_total = att_sum(att_total, blue, d)
+            else : blue_count += 1
+            corner, text = roll(blue)
+            stat[0][corner] += 1
+            if (corner == 0) : await message.channel.send(':regional_indicator_x: (miss)')
+            elif (die_pool[0] != 'hide') : await message.channel.send(text)
+            att_total = att_sum(att_total, blue, corner)
             continue
 
         elif ((dice == 'green') | (dice == 'g') | (dice == 'з')):
-            i = 0
-            d = random.randint(0,5)
-            stat[1][d] += 1
-            if (die_pool[0] != 'hide') : await message.channel.send(':green_square: G: {} :archery: + {} :heart: + {} :zap:'.format(green[d][0],green[d][1],green[d][2]))
-            att_total = att_sum(att_total, green, d)
+            corner, text = roll(green)
+            stat[1][corner] += 1
+            if (die_pool[0] != 'hide') : await message.channel.send(text)
+            att_total = att_sum(att_total, green, corner)
             continue
 
         elif ((dice == 'yellow') | (dice == 'y') | (dice == 'ж')):
-            i = 0
-            d = random.randint(0,5)
-            stat[2][d] += 1
-            if (die_pool[0] != 'hide') : await message.channel.send(':yellow_square: Y: {} :archery: + {} :heart: + {} :zap:'.format(yellow[d][0],yellow[d][1],yellow[d][2]))
-            att_total = att_sum(att_total, yellow, d)
+            corner, text = roll(yellow)
+            stat[2][corner] += 1
+            if (die_pool[0] != 'hide') : await message.channel.send(text)
+            att_total = att_sum(att_total, yellow, corner)
             continue
 
         elif ((dice == 'red') | (dice == 'r') | (dice == 'к')):
-            i = 0
-            d = random.randint(0,5)
-            stat[3][d] += 1
-            if (die_pool[0] != 'hide') : await message.channel.send(':red_square: R: {} :archery: + {} :heart: + {} :zap:'.format(red[d][0],red[d][1],red[d][2]))
-            att_total = att_sum(att_total, red, d)
+            corner, text = roll(red)
+            stat[3][corner] += 1
+            if (die_pool[0] != 'hide') : await message.channel.send(text)
+            att_total = att_sum(att_total, red, corner)
             continue
 
         elif ((dice == 'brown') | (dice == 'br') | (dice == 'зк')):
@@ -191,9 +218,7 @@ async def on_message(message):
         if (dice == 'stat-wipe'):
            if message.author.name != 'Хаёши': await message.channel.send('No-no-no, you are not the Master!')
            else :
-               stat = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
-               #pkl.dump(stat, stat_output, 2)
-               #stat_output.close()
+               #stat = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
                await message.channel.send('All statistics succesfully wiped!')
 
     if att_total != [0,0,0]: await message.channel.send('Total: {} :archery: + {} :heart: + {} :zap:'.format(att_total[0],att_total[1],att_total[2]))
@@ -201,5 +226,7 @@ async def on_message(message):
 
     pkl.dump(stat, stat_output, 2)
     stat_output.close()
+
+
 
 client.run(os.getenv("DISCORD_CLIENT_KEY"))
